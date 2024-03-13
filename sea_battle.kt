57c896f7fc80ef118635755fe1df82a122d615ba
landa-vadim <porvalrubahu@gmail.com>
2024-03-13 15:54:32 +0300

    add myField generate by One-Deck Ships

--- sea_battle.kt
+++ sea_battle.kt
@@ -1,11 +1,74 @@
 fun main() {
     var myField = Array(10) { Array(10) { 0 } }
     var pcField = Array(10) { Array(10) { 0 } }
-    var number1 = 1
     val letterArray = charArrayOf('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К')
 
-    topLine(letterArray)
+    printMyField(letterArray, myField)
 
+    var countShipsDecks = 0
+    val predicate: (Int) -> Boolean = { it == 1 }
+
+
+    while (countShipsDecks < 3) {
+        if (createOneDeckShip(myField)) {
+            countShipsDecks++
+        } else {
+            continue
+        }
+    }
+
+    printMyField(letterArray, myField)
+
+}
+
+fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
+
+    val r1 = (0..9).random()
+    val r2 = (0..9).random()
+
+    for (r in (r1 - 1..r1 + 1)) {
+        for (l in (r2 - 1..r2 + 1)) {
+            if (r1 - 1 >= 0 && r2 - 1 >= 0 && r1 + 1 <= 9 && r2 + 1 <= 9) {
+                if (myField[r][l] == 0) {
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+
+    for (r in (r1 - 1..r1 + 1)) {
+        for (l in (r2 - 1..r2 + 1)) {
+            if (r1 - 1 >= 0 && r2 - 1 >= 0 && r1 + 1 <= 9 && r2 + 1 <= 9) {
+                if (r != r1 || l != r2) {
+                    myField[r][l] = 2
+                }
+                if (r == r1 && l == r2) {
+                    myField[r][l] = 1
+                }
+            } else {
+                continue
+            }
+
+        }
+    }
+    return true
+}
+
+
+fun printMyField(letterArray: CharArray, myField: Array<Array<Int>>) {
+
+    var number0 = 0
+    var number1 = 1
+
+    println()
+    print("    ")
+    for (i in letterArray) {
+        print(letterArray[number0++])
+        print(" ")
+    }
+    println()
     for (row in myField) {
 
         if (number1 < 10) {
@@ -13,66 +76,17 @@ fun main() {
         } else {
             print("$number1|")
         }
-
         print(" ")
+
         for (line in row) {
 
             print("$line ")
         }
         println()
-
-    }
-
-
-//while ()
-
-    var random1_1 = (0..9).random()
-    var random1_2 = (0..9).random()
-
-    if (myField[random1_1 - 1][random1_2 - 1] == 0) {
-        if (myField[random1_1 - 1][random1_2] == 0) {
-            if (myField[random1_1 - 1][random1_2 + 1] == 0) {
-                if (myField[random1_1][random1_2 + 1] == 0) {
-                    if (myField[random1_1 + 1][random1_2 + 1] == 0) {
-                        if (myField[random1_1 + 1][random1_2] == 0) {
-                            if (myField[random1_1 + 1][random1_2 - 1] == 0) {
-                                if (myField[random1_1][random1_2 - 1] == 0) {
-
-//                                } else continue
-//                            }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-
-            myField[random1_1][random1_2] = 1
-
-        }
     }
 }
+//fun topLine(letterArray: CharArray) {
 
-fun getRandomA(): Int {
-    var a = (0..9).random()
-    return a
-}
-
-fun getRandomB(): Int {
-    var b = (0..9).random()
-    return b
-}
-
-fun topLine(letterArray: CharArray) {
-    var number0 = 0
-    print("    ")
-    for (i in letterArray) {
-        print(letterArray[number0++])
-        print(" ")
-    }
-    println()
-}
 //    print(letterArray[1])
 //    print(" ")
 //    print(letterArray[2])
@@ -91,4 +105,71 @@ fun topLine(letterArray: CharArray) {
 //    print(" ")
 //    print(letterArray[9])
 //    println()
+//}
+
+//fun getRandomA(): Int {
+//    var a = (0..9).random()
+//    return a
+//}
+//
+//fun getRandomB(): Int {
+//    var b = (0..9).random()
+//    return b
+//}
+//    if (myField[random1_1][random1_2] == 0) {
+//        if (myField[random1_1 - 1][random1_2 - 1] == 0) {
+//            if (myField[random1_1 - 1][random1_2] == 0) {
+//                if (myField[random1_1 - 1][random1_2 + 1] == 0) {
+//                    if (myField[random1_1][random1_2 + 1] == 0) {
+//                        if (myField[random1_1 + 1][random1_2 + 1] == 0) {
+//                            if (myField[random1_1 + 1][random1_2] == 0) {
+//                                if (myField[random1_1 + 1][random1_2 - 1] == 0) {
+//                                    if (myField[random1_1][random1_2 - 1] == 0) {
+//                                        myField[random1_1][random1_2] = 1
+//                                        myField[random1_1 - 1][random1_2 - 1] = 2
+//                                        myField[random1_1 - 1][random1_2] = 2
+//                                        myField[random1_1 - 1][random1_2 + 1] = 2
+//                                        myField[random1_1][random1_2 + 1] = 2
+//                                        myField[random1_1 + 1][random1_2 + 1] = 2
+//                                        myField[random1_1 + 1][random1_2] = 2
+//                                        myField[random1_1 + 1][random1_2 - 1] = 2
+//                                        myField[random1_1][random1_2 - 1] = 2
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return true
+//    } else return false
+
+//fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
+//
+//    val r1 = (0..9).random()
+//    val r2 = (0..9).random()
+//
+//    for (r in myField[r1 - 1][r2]..myField[r1 + 1][r2]) {
+//        for (l in myField[r][r2 - 1]..myField[r][r2 + 1]) {
+//            if (r1 - 1 >= 0 && r2 - 1 >= 0) {
+//                if (l == 0) {
+//                    continue
+//                } else {
+//                    return false
+//                }
+//            } else {
+//                continue
+//            }
+//        }
+//    }
+//
+//    for (r in myField[r1 - 1][r2]..myField[r1 + 1][r2]) {
+//        for (l in myField[r][r2 - 1]..myField[r][r2 + 1]) {
+//            if (r1 - 1 >= 0 && r2 - 1 >= 0) {
+//                myField[r][l] = 2
+//            }
+//        }
+//    }
+//    return true
 //}
